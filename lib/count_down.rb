0de4d6ffde9b585e360e71c147f512b64ae8d59c
Ekene N <ekenayy@gmail.com>
2020-10-02 21:11:42 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,9 @@
 # Write your code here
 count = 10
 
-while count > 1 do
+while count >= 1 do
  puts "#{count}"
- #else if count == 1 
-   #puts "Happy New Year!"
+ if count == 1 
+   puts "Happy New Year!"
    count -= 1
  end
